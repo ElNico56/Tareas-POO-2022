01902c97abe4ab5b9e4755381447673884b93505
ElNico56 <meeqnico@gmail.com>
2022-10-31 22:39:24 -0300

    Update botilleria.py

--- botilleria.py
+++ botilleria.py
@@ -5,20 +5,44 @@ class Product:
 	stock = 0 # cantidad en stock
 	critical_stock = 0 # cantidad critica
 	price = 0 # precio en pesos
+	retail_price = 0 # precio de venta
+	expiration_date = 0 # fecha de vencimiento
 	supplier = "" # proveedor
-	notes = "" # observaciones miscelaneas
+	notes = "" # observaciones
 
 	## Constructor ##
-	def __init__(self, id, name, price)
+	def __init__(self, id, name, price):
 		self.id = id
 		self.name = name
 		self.price = price
 
 	## Metodos ##
-	def set_price(self, price):
+	def update_price(self, price):
 		self.price = price
 
 	def add_stock(self, ammount):
 		self.stock += ammount
 
-	def f(x):
+	def set_note(self, note):
+		self.note = note
+
+class Supplier:
+	## Atributos ##
+	id # identificador unico
+	name = "" # nombre
+	rut = "" # rol unico trubutario
+	address = "" # dirección
+	notes = "" # observaciones
+
+	## Constructor ##
+	def __init__(self, id, name, rut):
+		self.id = id
+		self.name = name
+		self.rut = rut
+
+	## Metodos ##
+	def update_address(self, address):
+		self.address = address
+
+	def set_note(self, note):
+		self.note = note
