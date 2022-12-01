8077d90218b97fbf63febe03db480a2cd8e98989
ElNico56 <meeqnico@gmail.com>
2022-12-01 09:23:49 -0300

    priv

--- ficha.py
+++ ficha.py
@@ -1,20 +1,20 @@
 class MedicalRecord:
     def __init__(self, name, bday, weight, height):
-        self.name = name
-        self.birthday = bday  # YYYY/MM/DD
-        self.weight = weight  # in meters
-        self.height = height  # in kg
-        self.history = ""
+        self._name = name
+        self._birthday = bday  # YYYY/MM/DD
+        self._weight = weight  # in meters
+        self._height = height  # in kg
+        self._history = ""
 
     def add_history(self, string):
-        self.history += "\n	" + string
+        self._history += "\n	" + string
 
     def display(self):
-        print("Paciente: " + self.name)
-        print("- Fecha nacimiento: " + self.birthday)
-        print("- Altura: " + str(self.height))
-        print("- Peso: " + str(self.weight))
-        print("- Historial medico: " + self.history)
+        print("Paciente: " + self._name)
+        print("- Fecha nacimiento: " + self._birthday)
+        print("- Altura: " + str(self._height))
+        print("- Peso: " + str(self._weight))
+        print("- Historial medico: " + self._history)
 
 
 paciente = MedicalRecord("Vincent Vicson", "1995/04/24", 75, 1.75)
